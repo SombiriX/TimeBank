1188a864b7fece1dd4ae74cfdd5f3206a0688442
Sombiri Enwemeka <sombiri@enwemeka.com>
2018-09-01 13:34:39 -0700

    Added dummy code for timebank view

---
+++ timebank_frontend/src/views/TimeBank.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+      <v-card>
+        <v-card-title primary-title>
+          Title
+        </v-card-title>
+        <v-card-text>
+          Text
+        </v-card-text>
+      </v-card>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
