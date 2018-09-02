2833c20027e9864c77059138f59727e615487b28
Sombiri Enwemeka <sombiri@enwemeka.com>
2018-09-02 03:46:13 +0000

    Added vuelidate input validation for registration form

--- timebank_frontend/src/views/Register.vue
+++ timebank_frontend/src/views/Register.vue
@@ -16,16 +16,20 @@
             >
               <v-text-field
                 label="Email"
-                :rules="[rules.required]"
                 required
                 v-model="inputs.email"
+                @input="$v.inputs.email.$touch()"
+                @blur="$v.inputs.email.$touch()"
+                :error-messages="emailErrors"
               >
               </v-text-field>
               <v-text-field
                 label="Username"
-                :rules="[rules.required]"
                 required
                 v-model="inputs.username"
+                @input="$v.inputs.username.$touch()"
+                @blur="$v.inputs.username.$touch()"
+                :error-messages="usernameErrors"
               >
               </v-text-field>
               <v-text-field
@@ -35,9 +39,11 @@
                 :append-icon="pwd_visibility ? 'visibility' : 'visibility_off'"
                 @click:append="() => (pwd_visibility = !pwd_visibility)"
                 value="Password"
-                :rules="[rules.required, rules.atLeast8]"
                 :type="pwd_visibility ? 'text' : 'password'"
                 v-model="inputs.password1"
+                @input="$v.inputs.password1.$touch()"
+                @blur="$v.inputs.password1.$touch()"
+                :error-messages="pass1Errors"
               >
               </v-text-field>
               <v-text-field
@@ -47,9 +53,11 @@
                 :append-icon="pwd_visibility ? 'visibility' : 'visibility_off'"
                 @click:append="() => (pwd_visibility = !pwd_visibility)"
                 value="Password"
-                :rules="[rules.required, rules.atLeast8, rules.passMatch]"
                 :type="pwd_visibility ? 'text' : 'password'"
                 v-model="inputs.password2"
+                @input="$v.inputs.password2.$touch()"
+                @blur="$v.inputs.password2.$touch()"
+                :error-messages="pass2Errors"
               >
               </v-text-field>
               <v-container grid-list-md text-xs-center>
@@ -102,9 +110,13 @@
 </template>
 
 <script>
+import { validationMixin } from 'vuelidate'
+import { required, maxLength, minLength, email } from 'vuelidate/lib/validators'
+
 import { mapActions, mapState } from 'vuex'
 
 export default {
+  mixins: [validationMixin],
   data () {
     return {
       inputs: {
@@ -113,23 +125,64 @@ export default {
         password2: '',
         email: ''
       },
-      pwd_visibility: false,
-      rules: {
-        required: value => value.length > 0 || 'This field is required',
-        atLeast8: value => (
-          value.length >= 8 || 'At least 8 characters required'
-        ),
-        passMatch: value => (
-          value === this.inputs.password1 || 'Passwords do not match'
-        )
-      }
+      pwd_visibility: false
     }
   },
-  computed: mapState('signup', [
-    'registrationCompleted',
-    'registrationError',
-    'registrationLoading'
-  ]),
+  validations: {
+    inputs: {
+      username: { required, maxLength: maxLength(100) },
+      password1: { required, minLength: minLength(8) },
+      password2: { required, minLength: minLength(8)},
+      email: { required, email }
+    }
+  },
+  computed: Object.assign({},
+    mapState('signup', [
+      'registrationCompleted',
+      'registrationError',
+      'registrationLoading'
+    ]),
+    {
+      usernameErrors () {
+        const errors = []
+        if (!this.$v.inputs.username.$dirty) return errors
+        !this.$v.inputs.username.maxLength && errors.push(
+          'Username must be at most 100 characters long')
+        !this.$v.inputs.username.required && errors.push(
+          'Username is required.')
+        return errors
+      },
+      emailErrors () {
+        const errors = []
+        if (!this.$v.inputs.email.$dirty) return errors
+        !this.$v.inputs.email.email && errors.push('Must be valid e-mail')
+        !this.$v.inputs.email.required && errors.push('E-mail is required')
+        return errors
+      },
+      pass1Errors () {
+        const errors = []
+        if (!this.$v.inputs.password1.$dirty) return errors
+        !this.$v.inputs.password1.minLength && errors.push(
+          'At least 8 characters required')
+        !this.$v.inputs.password1.required && errors.push(
+          'Password is required.')
+        return errors
+      },
+      pass2Errors () {
+        const errors = []
+        let confirmPass = this.inputs.password2
+        let otherPass = this.inputs.password1
+        if (!this.$v.inputs.password2.$dirty) return errors
+        !this.$v.inputs.password2.minLength && errors.push(
+          'At least 8 characters required')
+        !this.$v.inputs.password2.required && errors.push(
+          'Password is required.')
+        !(confirmPass === otherPass) && errors.push(
+          'Passwords do not match')
+        return errors
+      }
+    }
+  ),
   methods: Object.assign({},
     mapActions('signup', [
       'createAccount',
@@ -138,6 +191,7 @@ export default {
     {
       submit: function () {
         // Validate inputs and login
+        this.$v.$touch()
         if (this.$refs.registerForm.validate()) {
           var inputs = this.inputs
           this.createAccount(inputs)
