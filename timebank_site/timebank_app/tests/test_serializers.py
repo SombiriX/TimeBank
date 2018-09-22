9529b8747f7902001a284bb72b41e6c1771b2222
Sombiri Enwemeka <sombiri@enwemeka.com>
2018-09-22 16:27:04 -0700

    #1 Added serializer tests for running interval

--- timebank_site/timebank_app/tests/test_serializers.py
+++ timebank_site/timebank_app/tests/test_serializers.py
@@ -1,4 +1,5 @@
 from django.test import RequestFactory, TestCase
+from django.utils import timezone
 from rest_framework.relations import Hyperlink
 
 from timebank_app.models import (
@@ -87,14 +88,6 @@ class IntervalSerializerTest(TestCase):
             'task': self.task,
             'created': '',
             'last_modified': '',
-        }
-
-        self.serializer_data = {
-            'start': '2017-09-18T06:14:37.712159Z',
-            'stop': '2017-09-18T07:14:37.712159Z',
-            'task': self.task,
-            'created': '2017-09-18T06:14:37.712159Z',
-            'last_modified': '2017-09-18T06:14:37.712159Z',
         }
 
         self.interval = Interval.objects.create(**self.interval_attributes)
@@ -188,63 +181,137 @@ class TaskSerializerTest(TestCase):
             type(data['author']),
             Hyperlink
         )
+
     def test_complete_field_content(self):
         data = self.serializer.data
         self.assertEqual(
             data['complete'],
             self.task_attributes['complete']
         )
+
     def test_created_field_content(self):
         data = self.serializer.data
         self.assertEqual(
             type(data['created']),
             str
         )
+
     def test_last_added_field_content(self):
         data = self.serializer.data
         self.assertEqual(
             data['last_added'],
             self.task_attributes['last_added']
         )
+
     def test_last_modified_field_content(self):
         data = self.serializer.data
         self.assertEqual(
             type(data['last_modified']),
             str
         )
+
     def test_parent_task_field_content(self):
         data = self.serializer.data
         self.assertEqual(
             data['parent_task'],
             self.task_attributes['parent_task']
         )
+
     def test_running_field_content(self):
         data = self.serializer.data
         self.assertEqual(
             data['running'],
             self.task_attributes['running']
         )
+
     def test_task_name_field_content(self):
         data = self.serializer.data
         self.assertEqual(
             data['task_name'],
             self.task_attributes['task_name']
         )
+
     def test_task_notes_field_content(self):
         data = self.serializer.data
         self.assertEqual(
             data['task_notes'],
             self.task_attributes['task_notes']
         )
+
     def test_task_type_field_content(self):
         data = self.serializer.data
         self.assertEqual(
             data['task_type'],
             self.task_attributes['task_type']
         )
+
     def test_time_budget_field_content(self):
         data = self.serializer.data
         self.assertEqual(
             data['time_budget'],
             self.task_attributes['time_budget']
         )
+
+    def test_task_running(self):
+        running_task = Task.objects.create(**self.task_attributes)
+
+        serializer1 = TaskSerializer(
+            instance=running_task,
+            context={'request': self.request}
+        )
+        data1 = serializer1.data
+        right_now = timezone.now()
+        an_hour_ago = right_now - timezone.timedelta(hours=1)
+        thirty_mins_ago = right_now - timezone.timedelta(minutes=30)
+        fifteen_mins_ago = right_now - timezone.timedelta(minutes=15)
+
+        interval1 = Interval.objects.create(
+            task=running_task,
+            start=an_hour_ago,
+            stop=thirty_mins_ago
+        )
+        interval2 = Interval.objects.create(
+            task=running_task,
+            start=fifteen_mins_ago
+        )
+
+        running_task.running = True
+        running_task.save()
+
+        serializer2 = TaskSerializer(
+            instance=running_task,
+            context={'request': self.request})
+        data2 = serializer2.data
+
+        # Assert that time delta is within 1 second of 45 minutes
+        self.assertAlmostEqual(
+            data2['runtime'] - data1['runtime'],
+            2700,
+            delta=1
+        )
+
+        # Verify the running interval
+        i1 = IntervalSerializer(instance=interval1)
+        i2 = IntervalSerializer(instance=interval2)
+        self.assertEqual(
+            data2['running_interval'],
+            i2.data
+        )
+        self.assertNotEqual(
+            data1['running_interval'],
+            i1.data
+        )
+
+        # Stop the running task and verify that running_interval is None
+        interval2.stop = right_now
+        interval2.save()
+
+        running_task.running = False
+        running_task.save()
+
+        serializer3 = TaskSerializer(
+            instance=running_task,
+            context={'request': self.request})
+        data3 = serializer3.data
+
+        self.assertIsNone(data3['running_interval'])
