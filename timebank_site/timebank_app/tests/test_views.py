64abb76e837b4a82b26c85c818d9309ae9bde089
Sombiri Enwemeka <sombiri@enwemeka.com>
2018-09-23 16:06:58 -0700

    #1 added tests for current_user view

--- timebank_site/timebank_app/tests/test_views.py
+++ timebank_site/timebank_app/tests/test_views.py
@@ -1,3 +1,77 @@
-from django.test import TestCase
+from django.db.models import F
+from django.urls import reverse
+from django.utils import timezone
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, APITestCase
 
-# Create your tests here.
+from timebank_app.models import User
+from timebank_app.serializers import UserSerializer
+
+
+class CurrentUserViewTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        cls.user = User.objects.create_user(
+            username='testuser1',
+            password='12345'
+        )
+
+        cls.other_user1 = User.objects.create_user(
+            username='testuser2',
+            password='12345'
+        )
+
+        cls.other_user2 = User.objects.create_superuser(
+            username='superuser1',
+            password='12345',
+            email='super@example.com'
+        )
+
+        cls.factory = APIRequestFactory()
+
+    def setUp(self):
+        self.login = self.client.login(
+            username='testuser1',
+            password='12345'
+        )
+
+    def test_get_user(self):
+        test_pk = self.user.pk
+        test_url = reverse('currentUser')
+        request = self.factory.get(test_url)
+        response = self.client.get(test_url)
+        # get data from db
+        users = User.objects.all()
+        serializer = UserSerializer(
+            users,
+            context={'request': request},
+            many=True
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertIsInstance(response.data, dict)
+
+        field_names = [
+            'date_joined',
+            'email',
+            'first_name',
+            'id',
+            'last_login',
+            'last_name',
+            'user_prefs',
+            'username',
+        ]
+        self.assertCountEqual(response.data.keys(), field_names)
+
+        user_pref_field_names = [
+            'complete_anim',
+            'twentyFourClock',
+        ]
+        self.assertCountEqual(
+            response.data['user_prefs'].keys(),
+            user_pref_field_names
+        )
+
+        self.assertEqual(response.data['id'], test_pk)
