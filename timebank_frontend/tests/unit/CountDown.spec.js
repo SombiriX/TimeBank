c34bba09eab60751fdcae408e8731dc44dfdbb22
Sombiri Enwemeka <sombiri@enwemeka.com>
2018-09-24 04:22:15 +0000

    #4 added test for countdown component

--- timebank_frontend/tests/unit/CountDown.spec.js
+++ timebank_frontend/tests/unit/CountDown.spec.js
@@ -1,6 +1,7 @@
-import { expect } from 'chai';
-import { shallowMount } from '@vue/test-utils';
-import CountDown from '@/components/CountDown.vue';
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+import '../../src/plugins/vuetify'
+import CountDown from '@/components/CountDown.vue'
 
 describe('CountDown.vue', () => {
   it('Displays a 00:00 counter when not paused and not running', () => {
@@ -17,8 +18,29 @@ describe('CountDown.vue', () => {
         elapsedTime,
         running,
         paused
-      },
+      }
     })
     expect(wrapper.text()).to.include(timerText)
+  })
+
+  it('Decrements the clock while running', (done) => {
+    const data = {
+      twentyFourClock: false,
+      initialTime: 0,
+      elapsedTime: 0,
+      running: false,
+      paused: false
+    }
+    const wrapper = shallowMount(CountDown, {
+      propsData: data
+    })
+    wrapper.vm.elapsedTime = 50
+    wrapper.vm.running = true
+    // console.log(wrapper.emitted('countDownTick'))
+    // expect(wrapper.emitted('countDownTick')).to.be.true
+    wrapper.vm.$nextTick(() => {
+      expect(wrapper.vm.secondsLeft).to.be.above(0)
+      done()
+    })
   })
 })
