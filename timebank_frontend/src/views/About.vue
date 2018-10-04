584ad5d3d21561860e92dd7e59c408dbc9d17ad6
Sombiri Enwemeka <sombiri@enwemeka.com>
2018-10-04 02:56:22 +0000

    #19 Added some descriptions to the frontpage

--- timebank_frontend/src/views/About.vue
+++ timebank_frontend/src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container fluid>
+    <v-layout column align-center justify-center fill-height class="about">
+      <v-flex xs12>
+        <div class="body-2 about_text">
+          <span>Time Bank is a </span>
+          <span class="confirm_selection">simple </span>
+          <span>way allocate and track time for specific tasks without the tedium of attempting to schedule every second of the day</span>        
+        </div>
+      </v-flex>          
+    </v-layout>    
+  </v-container>
 </template>
+
+<style>.confirm_selection {
+    animation: glow .9s infinite alternate;
+}
+
+@keyframes glow {
+    to {
+        text-shadow: 0 0 10px #4caf50;
+    }
+}
+
+.confirm_selection {
+    font-family: sans-serif;
+    font-size: 18px;
+    font-weight: bold;
+}
+
+.about {
+  height: 500;
+}
+
+.about_text {
+  height: 100;
+}
+</style>
--- timebank_frontend/src/views/Home.vue
+++ timebank_frontend/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
-      </v-layout>
-    </v-slide-y-transition>
+    <v-layout column align-center class="display-4">
+      <v-flex xs1>
+        <transition name="slide">
+          <span>Simple.</span>
+        </transition>
+      </v-flex>
+      <v-flex xs1>
+        Efficient.
+      </v-flex>
+      <v-flex xs1>
+        Time Tracking.
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
